5092dcb8addaf1d63160125d2e0a8ea0ec5fe64e
Adrian-Valiente <a.valiente.2018@alumnos.urjc.es>
2020-11-12 13:14:46 +0100

    Se ha cambiado completamente el menú, pero no se ven las imágenes

--- Game/src/scenes/mainMenu.js
+++ Game/src/scenes/mainMenu.js
@@ -5,27 +5,61 @@ class MainMenu extends Phaser.Scene {
         super({ key: "MAINMENU" });
     }
     create(){
-        console.log("asdasdasd")
-        let bg= this.add.image(0,0,"mainmenu").setOrigin(0,0);
-        // let pb= this.add.image(this.sys.game.canvas.width/2,this.sys.game.canvas.height/2,"playButton");
-        // pb.setScale(0.3);
-        let pb= new Sprite(this,this.sys.game.canvas.width/2,this.sys.game.canvas.height/2,"playButton");
-        pb.setScale(0.3)
-        pb.setInteractive();
-
-        pb.on("pointerdown",()=>{
-            console.log("hola");
+        let bg= this.add.image(0,0,"menu").setOrigin(0,0);     
+        bg.displayWidth=this.game.canvas.width;  
+        bg.displayHeight=this.game.canvas.height;  
+        let container= document.getElementsByClassName('animated-button1');
+        
+        
+        
+        //container.addEventListener('click',()=>{});
+        //container.setOrigin(0,0);
             
+         
+         let hoverSprite=this.add.sprite(100,100,"FlagSheet2");
+         hoverSprite.setVisible(false);
+         let pb2=this.add.sprite(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, "Play");
+         let soundM=this.add.sprite(950, 75, "Mute");
+         //let pb= this.add.image(this.sys.game.canvas.width/2,this.sys.game.canvas.height/2,"playButton");
+
+         soundM.setScale(0.45);
+         soundM.setInteractive();
+
+        
+
+        soundM.on("pointerdown", ()=>{
+            console.log(soundM.frame.name);
+            soundM.frame.name=soundM.frame.name==0?soundM.setFrame(1):soundM.setFrame(0);           
         })
-        pb.on("pointerup",()=>{
+
+         
+        pb2.setScale(0.45); 
+        pb2.setOrigin(0.48,-0.1);
+        pb2.setInteractive();
+
+        pb2.on("pointerover", ()=>{
+            hoverSprite.setVisible(true);
+            hoverSprite.play("wave");
+            hoverSprite.x=pb2.x;
+            hoverSprite.y=pb2.y-20;
+            pb2.setFrame(1);
            
+        })
+
+        
+        pb2.on("pointerout", ()=>{
+            hoverSprite.setVisible(false);
+            pb2.setFrame(0);
+            
+        } )
+        
+        pb2.on("pointerup",()=>{            
+            /*this.scene.transition({
+                target: "Scene_Play",
+                duration:3000
+            })*/
             this.scene.start("Scene_play");
         })
-
-
-        console.log(this.scene.isActive(this.scene.get("Scene_play")))
-        //console.log(document.getElementById("contenedor").clientWidth);
-        //console.log(document.getElementById("contenedor").offsetWidth);
 
 
 
